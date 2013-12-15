9305ed0cbe9ff4234c14129583174e7db019efe0
Sven Bohm <bohms@msu.edu>
2013-12-15 15:50:37 -0500

    code from kalkaska

--- fft-filter.r
+++ fft-filter.r
@@ -1,6 +1,15 @@
 library(signal)
+library(plyr)
+
+daily <- function(x) { if (length(x) > 5) {median(x, na.rm=T) } else { NA}  }
+filter <- function(x) runmed(x, 13)
+
 data <- read.csv('data.csv', header=F)
-names(data) <- c('plot','depth','date','vwc')
-f <- fir1(64, 0.01, type='low', scale=TRUE)
-data$filtered <- runmed(data$vwc,13)
-write.csv(data,'filtered.csv', row.names=FALSE, col.names=FALSE)
+names(data) <- c('plot','depth','datetime','vwc')
+data$datetime <- as.POSIXct(data$datetime)
+
+data$date <- as.Date(format(data$datetime, '%Y-%m-%d'))
+filtered <- ddply(data, .(plot, depth), transform, filtered=filter(vwc))
+reduced <- ddply(filtered, .(plot, depth, date), summarize, daily=daily(filtered))
+
+write.csv(reduced[complete.cases(reduced),],'filtered.csv', row.names=FALSE, col.names=FALSE)
